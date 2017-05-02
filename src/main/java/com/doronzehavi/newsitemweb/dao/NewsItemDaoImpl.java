0a54443aa164d2b16169a82db855fac4bb547ac6
doronz <doron1zehavi@gmail.com>
2017-05-01 22:10:39 -0700

    trying to fix db

--- src/main/java/com/doronzehavi/newsitemweb/dao/NewsItemDaoImpl.java
+++ src/main/java/com/doronzehavi/newsitemweb/dao/NewsItemDaoImpl.java
@@ -1,13 +1,10 @@
 package com.doronzehavi.newsitemweb.dao;
 
 import com.doronzehavi.newsitemweb.model.item.NewsItem;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
