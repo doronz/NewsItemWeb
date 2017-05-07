d39ff6145842d6d264773487cd67e805eb9ebbff
doronz <doron1zehavi@gmail.com>
2017-05-07 12:39:38 -0700

    fixes to layout

--- src/main/java/com/doronzehavi/newsitemweb/controllers/MainController.java
+++ src/main/java/com/doronzehavi/newsitemweb/controllers/MainController.java
@@ -2,12 +2,13 @@ package com.doronzehavi.newsitemweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class MainController {
 
     @RequestMapping(value = "/")
-    public String showHome(){
-        return "home";
+    public RedirectView straighToFeed(){
+        return new RedirectView("feed");
     }
 }
