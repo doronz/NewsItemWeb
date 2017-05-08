f9a0416f30e1ec7c47c4352f93405b49fb5f868f
doronz <doron1zehavi@gmail.com>
2017-05-07 18:26:43 -0700

    fixed UI and attempting to debug remotely

--- src/main/java/com/doronzehavi/newsitemweb/controllers/FeedController.java
+++ src/main/java/com/doronzehavi/newsitemweb/controllers/FeedController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -16,18 +18,33 @@ public class FeedController {
     @Autowired
     private NewsItemService newsItemService;
 
-    @SuppressWarnings("unchecked")
     @RequestMapping(value = "/feed")
-    public String listNewsItems(Model model) {
+    public String showFeed(Model model) {
+        Page<NewsItem> page = newsItemService.fetchNewsItemsByPage(1);
+        model.addAttribute("newsfeed", page.getContent());
+        int current = page.getNumber();
+        int next = current + 1;
+        int last = page.getTotalPages() - 1;
 
-//        List<NewsItem> newsFeedList = newsItemService.fetchAllNewsItems();
-//        model.addAttribute("newsfeed", newsFeedList);
-        Page<NewsItem> newsFeedPage = newsItemService.fetchNewsItemsByPage(1);
-
-        model.addAttribute("newsfeed", newsFeedPage.getContent());
-
+        model.addAttribute("current", current);
+        model.addAttribute("next", next);
+        model.addAttribute("last", last);
         return "feed";
     }
 
+    @RequestMapping(value = "/feed/{pageNumber}", method = RequestMethod.GET)
+    public String showFeedByPage(@PathVariable Integer pageNumber, Model model) {
+        Page<NewsItem> page = newsItemService.fetchNewsItemsByPage(pageNumber);
+        int current = page.getNumber();
+        int next = current + 1;
+        int prev = current - 1;
+        int last = page.getTotalPages() - 1;
 
+        model.addAttribute("current", current);
+        model.addAttribute("next", next);
+        model.addAttribute("prev", prev);
+        model.addAttribute("last", last);
+
+        return "feed";
+    }
 }
