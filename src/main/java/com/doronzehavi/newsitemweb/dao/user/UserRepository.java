7bbb2bc5951c6dbc48ecdfffe921775fb06d11b4
doronz <doron1zehavi@gmail.com>
2017-05-12 22:18:01 -0700

    Creating users and configuring security

--- src/main/java/com/doronzehavi/newsitemweb/dao/user/UserRepository.java
+++ src/main/java/com/doronzehavi/newsitemweb/dao/user/UserRepository.java
@@ -4,6 +4,8 @@ import com.doronzehavi.newsitemweb.model.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
 @RepositoryRestResource(exported = false)
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
 }
